8e426480b1473c120ed1ce4fbace62dabb7d8305
James <32562886+18810817370@users.noreply.github.com>
2018-07-31 21:12:25 +0800

    9

--- src/me/zly/_3/LongestSubstringWithoutRepeatingCharacters.java
+++ src/me/zly/_3/LongestSubstringWithoutRepeatingCharacters.java
@@ -11,16 +11,19 @@ import java.util.HashSet;
 //        Given "bbbbb", the answer is "b", with the length of 1.
 //
 //        Given "pwwkew", the answer is "wke", with the length of 3.
+//
 // Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
 public class LongestSubstringWithoutRepeatingCharacters {
 
     public int lengthOfLongestSubstring(String s) {
+
         int lenth = 0;
+
         for (int i = 0; i < s.length(); i ++) {
             for (int j = i ; j < s.length(); j ++) {
                 String substr = s.substring(i, j + 1);
-                HashSet<Character> substrhs = new HashSet<Character>();
+                HashSet<Character> substrhs = new HashSet<>();
                 int l = substr.length();
                 for (int k = 0; k < l; k ++) {
                     substrhs.add(substr.charAt(k));
@@ -35,6 +38,7 @@ public class LongestSubstringWithoutRepeatingCharacters {
                 }
             }
         }
+
         return lenth;
     }
 
