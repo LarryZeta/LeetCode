55e0cbd260ff439a0213391fdbbf2e922076cc92
James <32562886+18810817370@users.noreply.github.com>
2018-08-04 22:29:45 +0800

    Remove Duplicates from Sorted Array

---
+++ src/me/zly/EasyCollection/Arry/RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,25 @@
+package me.zly.EasyCollection.Arry;
+
+public class RemoveDuplicatesfromSortedArray {
+
+    public int removeDuplicates(int[] nums) {
+
+        if (nums == null|| nums.length == 0) {
+            return 0;
+        }
+
+        int l = 1;
+        int value = nums[0];
+        for (int i = 1; i < nums.length; i ++) {
+            if (nums[i] != value) {
+                value = nums[i];
+                nums[l] = nums[i];
+                ++ l;
+            }
+        }
+
+        return l;
+
+    }
+
+}
