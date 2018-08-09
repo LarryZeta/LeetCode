bcf1d9682b68eb828dcd41c4d20be3e0fe98a4c9
James <32562886+18810817370@users.noreply.github.com>
2018-08-09 21:08:17 +0800

    add description

--- src/me/zly/Easy/_21/MergeTwoSortedLists.java
+++ src/me/zly/Easy/_21/MergeTwoSortedLists.java
@@ -1,5 +1,11 @@
 package me.zly.Easy._21;
 
+// Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+//
+// Example:
+//        Input: 1->2->4, 1->3->4
+//        Output: 1->1->2->3->4->4
+
 public class MergeTwoSortedLists {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
