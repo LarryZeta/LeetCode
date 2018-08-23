7f5d6f4d2a491ca6e921d847b01ada0792244351
James <32562886+18810817370@users.noreply.github.com>
2018-08-23 23:21:20 +0800

    46 TODO

---
+++ src/me/zly/Mideum/_46/Permutations.java
@@ -0,0 +1,29 @@
+package me.zly.Mideum._46;
+
+// Given a collection of distinct integers, return all possible permutations.
+//
+// Example:
+//        Input: [1,2,3]
+//        Output:
+//        [
+//          [1,2,3],
+//          [1,3,2],
+//          [2,1,3],
+//          [2,3,1],
+//          [3,1,2],
+//          [3,2,1]
+//        ]
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> lists = new ArrayList<>();
+
+        return lists;
+
+    }
+}
