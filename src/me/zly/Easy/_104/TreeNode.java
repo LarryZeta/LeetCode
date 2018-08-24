57b0a2e6611960f7d8a7a2d333fe701d2403d5fa
James <32562886+18810817370@users.noreply.github.com>
2018-08-24 15:40:36 +0800

    104

---
+++ src/me/zly/Easy/_104/TreeNode.java
@@ -0,0 +1,13 @@
+package me.zly.Easy._104;
+/**
+ * Definition for a binary tree node.
+ */
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+
+}
