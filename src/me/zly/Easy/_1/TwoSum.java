274861ae1a6a802915604b418ae443c7b3f01d25
James <32562886+18810817370@users.noreply.github.com>
2018-08-24 15:28:26 +0800

    1

--- src/me/zly/Easy/_1/TwoSum.java
+++ src/me/zly/Easy/_1/TwoSum.java
@@ -1,16 +1,20 @@
 package me.zly.Easy._1;
 
-// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-// You may assume that each input would have exactly one solution, and you may not use the same element twice.
-// 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
-// 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
-
-// Example:
-//
-//        Given nums = [2, 7, 11, 15], target = 9,
-//
-//        Because nums[0] + nums[1] = 2 + 7 = 9,
-//        return [0, 1].
+/**
+ * @author zly
+ *
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+ * 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+ *
+ * Example:
+ *
+ *        Given nums = [2, 7, 11, 15], target = 9,
+ *
+ *        Because nums[0] + nums[1] = 2 + 7 = 9,
+ *        return [0, 1].
+ */
 
 public class TwoSum {
 
