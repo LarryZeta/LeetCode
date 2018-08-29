1918a8223aa217d6e14ebf406665649933a8a9bc
James <32562886+18810817370@users.noreply.github.com>
2018-08-29 07:50:43 -0700

    110

--- src/me/zly/easy/_110/BalancedBinaryTree.java
+++ src/me/zly/easy/_110/BalancedBinaryTree.java
@@ -36,14 +36,23 @@ import me.zly.definition.TreeNode;
  * Return false.
  */
 
-// TODO
 public class BalancedBinaryTree {
 
     public boolean isBalanced(TreeNode root) {
 
         if (root == null) return true;
-        else if (isBalanced(root.left) && isBalanced(root.right)) return true;
-        else return false;
+
+        else if (Math.abs(getDepth(root.left) - getDepth(root.right)) > 1) return false;
+
+        else return isBalanced(root.left) && isBalanced(root.right);
+
+    }
+
+    public int getDepth(TreeNode root) {
+
+        if (root == null) return 0;
+
+        else return  1 + Math.max(getDepth(root.left), getDepth(root.right));
 
     }
 
