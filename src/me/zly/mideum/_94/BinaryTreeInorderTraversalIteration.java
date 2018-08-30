ffd75d20825f129449056cb418709e2e7d5795a5
James <32562886+18810817370@users.noreply.github.com>
2018-08-30 18:27:35 +0800

    94

---
+++ src/me/zly/mideum/_94/BinaryTreeInorderTraversalIteration.java
@@ -0,0 +1,35 @@
+package me.zly.mideum._94;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import me.zly.definition.TreeNode;
+
+/**
+ * @author zly
+ *
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ *
+ * Example:
+ *
+ * Input: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * Output: [1,3,2]
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+public class BinaryTreeInorderTraversalIteration {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+       return null;
+
+    }
+
+
+}
