2a06db36d93e56a22b527118a20a1283ad79c511
James <32562886+18810817370@users.noreply.github.com>
2018-09-06 12:51:56 +0800

    31 TODO

---
+++ src/me/zly/mideum/_31/NextPermutation.java
@@ -0,0 +1,30 @@
+package me.zly.mideum._31;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * @author zly
+ *
+ * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ *
+ * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+ *
+ * The replacement must be in-place and use only constant extra memory.
+ *
+ * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+ *
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ */
+
+// TODO
+public class NextPermutation {
+
+    public void nextPermutation(int[] nums) {
+
+
+    }
+
+}
