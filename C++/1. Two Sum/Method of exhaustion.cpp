b4ff4cd32faff87c0bca0c47a1b1217e7478ebbd
James <32562886+18810817370@users.noreply.github.com>
2018-09-15 12:40:55 +0800

    C++

---
+++ C++/1. Two Sum/Method of exhaustion.cpp
@@ -0,0 +1,35 @@
+﻿//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+//你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+
+//Example:
+//
+//        Given nums = [2, 7, 11, 15], target = 9,
+//
+//        Because nums[0] + nums[1] = 2 + 7 = 9,
+//        return [0, 1].
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target)
+	{
+		vector<int> r;
+		for (int i = 0; i < nums.size() - 1; i++)
+		{
+			for (int j = i + 1; j < nums.size(); j++)
+			{
+				if (nums.at(i) + nums.at(j) == target)
+				{
+					r.push_back(i);
+					r.push_back(j);
+					return r;
+				}
+			}
+		}
+		return r;
+	}
+};
---
+++ C++/2. Add Two Numbers/Add by digits.cpp
@@ -0,0 +1,62 @@
+﻿// You are given two non-empty linked lists representing two non-negative integers. The digits
+// are stored in reverse order and each of their nodes contain a single digit. Add the two
+// numbers and return it as a linked list. You may assume the two numbers do not contain any
+// leading zero, except the number 0 itself.
+//
+// 给定两个非空链表来表示两个非负整数。位数按照逆序方式存储，它们的每个节点只存储单个数字。将两数相加返回一个新的链表。
+// 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+//
+// Example:
+//
+//        Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+//        Output: 7 -> 0 -> 8
+//        Explanation: 342 + 465 = 807.
+
+#define NULL 0
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+	{
+		ListNode* l3 = new ListNode(l1->val + l2->val);
+		ListNode* l = l3;
+		int flag = 0;
+		while (l1 != NULL || l2 != NULL) 
+		{
+			flag = 0;
+			if (l->val >= 10) 
+			{
+				flag = 1;
+				l->val %= 10;
+			}
+			if (l1 != NULL)
+				l1 = l1->next;
+			if (l2 != NULL)
+				l2 = l2->next;
+			if (l1 != NULL && l2 != NULL) 
+			{
+				l->next = new ListNode(l1->val + l2->val + flag);
+			}
+			else if (l1 == NULL && l2 != NULL) 
+			{
+				l->next = new ListNode(l2->val + flag);
+			}
+			else if (l2 == NULL && l1 != NULL) 
+			{
+				l->next = new ListNode(l1->val + flag);
+			}
+			if (l->next != NULL)
+				l = l->next;
+		}
+		if (flag == 1)
+			l->next = new ListNode(1);
+		return l3;
+	}
+
+};
