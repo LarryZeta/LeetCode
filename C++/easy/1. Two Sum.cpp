32481c595903477753c7e04bdd8379c6a17a3c75
James <32562886+18810817370@users.noreply.github.com>
2018-09-26 16:28:55 +0800

    add date

--- C++/easy/1. Two Sum.cpp
+++ C++/easy/1. Two Sum.cpp
@@ -1,9 +1,8 @@
-﻿#include <vector>
-using namespace std;
+﻿//
+// Created by zly on 7/26/2018.
+//
 
 /**
- * @author zly
- *
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  *
@@ -14,6 +13,9 @@ using namespace std;
  *        Because nums[0] + nums[1] = 2 + 7 = 9,
  *        return [0, 1].
  */
+
+#include <vector>
+using namespace std;
 
 class Solution {
 public:
