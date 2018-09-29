e9ba2ab3d6c14ffd5517235c163b2df0f644d668
James <32562886+18810817370@users.noreply.github.com>
2018-09-29 20:16:03 +0800

    update: 172

---
+++ Java/src/me/zly/easy/_172/FactorialTrailingZeroesRecursive.java
@@ -0,0 +1,27 @@
+package me.zly.easy._172;
+
+/**
+ * @author zly
+ *
+ *Given an integer n, return the number of trailing zeroes in n!.
+ *
+ * Example 1:
+ *
+ * Input: 3
+ * Output: 0
+ * Explanation: 3! = 6, no trailing zero.
+ * Example 2:
+ *
+ * Input: 5
+ * Output: 1
+ * Explanation: 5! = 120, one trailing zero.
+ * Note: Your solution should be in logarithmic time complexity.
+ */
+
+public class FactorialTrailingZeroesRecursive {
+
+    public int trailingZeroes(int n) {
+        return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
+    }
+
+}
