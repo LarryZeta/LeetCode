21f5a7688d4c2886a119c800be0e2aa3fbe16844
James <32562886+18810817370@users.noreply.github.com>
2018-10-20 23:03:37 +0800

    Update: 234 TODO Optimize to O(1) space

---
+++ Java/src/me/zly/easy/_234/PalindromeLinkedList.java
@@ -0,0 +1,46 @@
+package me.zly.easy._234;
+
+import me.zly.definition.ListNode;
+
+import java.util.Stack;
+
+/**
+ * @author zly
+ *
+ * Given a singly linked list, determine if it is a palindrome.
+ *
+ * Example 1:
+ *
+ * Input: 1->2
+ * Output: false
+ * Example 2:
+ *
+ * Input: 1->2->2->1
+ * Output: true
+ * Follow up:
+ * Could you do it in O(n) time and O(1) space?
+ */
+
+public class PalindromeLinkedList {
+
+    public boolean isPalindrome(ListNode head) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        ListNode node = head;
+        while (node != null) {
+            stack.push(node.val);
+            node = node.next;
+        }
+
+        node = head;
+        while (!stack.isEmpty()) {
+            if (node.val != stack.pop()) return false;
+            node = node.next;
+        }
+
+        return true;
+
+    }
+
+}
