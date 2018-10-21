89478feed2d6fd5522ba9791a083d06f6068f9e5
James <32562886+18810817370@users.noreply.github.com>
2018-10-21 14:23:25 +0800

    Update: 268 beats 11.15%

---
+++ Java/src/me/zly/easy/_268/MissingNumber.java
@@ -0,0 +1,35 @@
+package me.zly.easy._268;
+
+import java.util.Arrays;
+
+/**
+ * @author zly
+ *
+ * Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+ *
+ * Example 1:
+ *
+ * Input: [3,0,1]
+ * Output: 2
+ * Example 2:
+ *
+ * Input: [9,6,4,2,3,5,7,0,1]
+ * Output: 8
+ * Note:
+ * Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+ */
+
+public class MissingNumber {
+
+    public int missingNumber(int[] nums) {
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i ++)
+            if (i != nums[i]) return i;
+
+        return -1;
+
+    }
+
+}
