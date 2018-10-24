6b2de05f8b974e059d5c3f86a8e13f505b4be618
James <32562886+18810817370@users.noreply.github.com>
2018-10-24 20:54:19 +0800

    Update: 204 beats 27.29%

---
+++ Java/src/me/zly/easy/_204/CountPrimes.java
@@ -0,0 +1,36 @@
+package me.zly.easy._204;
+
+import java.util.Arrays;
+
+/**
+ * @author zly
+ *
+ * Count the number of prime numbers less than a non-negative number, n.
+ *
+ * Example:
+ *
+ * Input: 10
+ * Output: 4
+ * Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+ */
+
+public class CountPrimes {
+
+    public int countPrimes(int n) {
+
+        boolean[] prime = new boolean[n];
+        Arrays.fill(prime, true);
+
+        for (int i = 2; i < n; i ++)
+            if (prime[i])
+                for (int j = i * 2; j < n; j = j + i) prime[j] = false;
+
+        int count = 0;
+        for (int i = 2; i < n; i ++)
+            if (prime[i]) count ++;
+
+        return count;
+
+    }
+
+}
