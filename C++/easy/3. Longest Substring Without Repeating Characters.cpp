4aa9e577dc82e2abb018b212c9b9bb4a485b643e
James <32562886+18810817370@users.noreply.github.com>
2018-11-08 10:40:13 +0800

    Update: 3 beats 65.61%

--- C++/easy/3. Longest Substring Without Repeating Characters.cpp
+++ C++/easy/3. Longest Substring Without Repeating Characters.cpp
@@ -9,3 +9,28 @@
 //        Given "pwwkew", the answer is "wke", with the length of 3.
 //
 // Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+#include <string>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        int size = s.size();
+
+        set<char> char_set;
+
+        int ans = 0, i = 0, j = 0;
+
+        while (i < size && j < size) {
+            if (char_set.count(s[j]) == 0) {
+                char_set.insert(s[j ++]);
+                ans = max(ans, j - i);
+            } else char_set.erase(s[i ++]);
+        }
+
+        return ans;
+
+    }
+};
