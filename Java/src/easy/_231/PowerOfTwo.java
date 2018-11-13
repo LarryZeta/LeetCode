6d3543f30feed90158b03d9994adea0fc5f79005
James <32562886+18810817370@users.noreply.github.com>
2018-11-13 22:55:57 +0800

    Update: 231 TODO

---
+++ Java/src/easy/_231/PowerOfTwo.java
@@ -0,0 +1,18 @@
+package easy._231;
+
+/**
+ * @author zly
+ *
+ * Given an integer, write a function to determine if it is a power of two.
+ *
+ * Credits:
+ * Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+ */
+
+public class PowerOfTwo {
+
+    public boolean isPowerOfTwo(int n) {
+        return true;
+    }
+
+}
