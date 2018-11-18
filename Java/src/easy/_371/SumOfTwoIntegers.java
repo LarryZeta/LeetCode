ae2f637f5e01ba370586e10ff6a6da893ce3c0f4
James <32562886+18810817370@users.noreply.github.com>
2018-11-18 19:43:13 +0800

    Create SumOfTwoIntegers.java

---
+++ Java/src/easy/_371/SumOfTwoIntegers.java
@@ -0,0 +1,26 @@
+package easy._371;
+
+/**
+ * @author zly
+ *
+ * Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+ *
+ * Example:
+ * Given a = 1 and b = 2, return 3.
+ *
+ * Credits:
+ * Special thanks to @fujiaozhu for adding this problem and creating all test cases.
+ */
+
+public class SumOfTwoIntegers {
+
+    public int getSum(int a, int b) {
+
+        int xor = a ^ b;
+        int and = a & b;
+        if (and != 0) return getSum(xor, and << 1);
+        else return xor;
+
+    }
+
+}
