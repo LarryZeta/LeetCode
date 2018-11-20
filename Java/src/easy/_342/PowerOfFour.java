181803ea18f8e7a633c612f7d09ec8db28feafb4
James <32562886+18810817370@users.noreply.github.com>
2018-11-20 22:37:42 +0800

    Create PowerOfFour.java

---
+++ Java/src/easy/_342/PowerOfFour.java
@@ -0,0 +1,23 @@
+package easy._342;
+
+/**
+ * @author zly
+ *
+ * Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+ *
+ * Example:
+ * Given num = 16, return true. Given num = 5, return false.
+ *
+ * Follow up: Could you solve it without loops/recursion?
+ *
+ * Credits:
+ * Special thanks to @yukuairoy for adding this problem and creating all test cases.
+ */
+
+public class PowerOfFour {
+
+    public boolean isPowerOfFour(int num) {
+        return num > 0 && (num & num - 1) == 0 && (num & 0xAAAAAAAA) == 0;
+    }
+
+}
