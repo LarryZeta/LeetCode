5d5bfa6e0d024ef458f06389621ae9da923096b2
LarryZeta <i@larryzeta.cc>
2023-04-20 16:33:26 +0800

    :triangular_flag_on_post: 70 dp solution

--- Java/src/easy/_70/ClimbingStairsDP.java
+++ Java/src/easy/_70/ClimbingStairsDP.java
@@ -2,29 +2,25 @@ package easy._70;
 
 /**
  * @author Larry
- *
- * You are climbing a stair case. It takes n steps to reach to the top.
- *
- * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
- *
- * Note: Given n will be a positive integer.
- *
- * Example 1:
- *        Input: 2
- *        Output: 2
- *        Explanation: There are two ways to climb to the top.
- *        1. 1 step + 1 step
- *        2. 2 steps
- *
- * Example 2:
- *        Input: 3
- *        Output: 3
- *        Explanation: There are three ways to climb to the top.
- *        1. 1 step + 1 step + 1 step
- *        2. 1 step + 2 steps
- *        3. 2 steps + 1 step
+ * <a href="https://leetcode.cn/problems/climbing-stairs/">LeetCode</a>
  */
 
-// TODO
 public class ClimbingStairsDP {
+
+    public int climbStairs(int n) {
+        // t[i] = t[i-1] + t[i-2]
+        int[] dp = new int[3];
+
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i = 3; i <= n; i ++) {
+            dp[2] = dp[0] + dp[1];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+
+        return dp[2];
+    }
+
 }
