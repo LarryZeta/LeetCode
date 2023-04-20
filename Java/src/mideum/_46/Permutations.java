f42606b8749dc0c71aa208df344ab19df621d720
LarryZeta <i@larryzeta.cc>
2023-04-20 18:10:57 +0800

    :triangular_flag_on_post: 46

--- Java/src/mideum/_46/Permutations.java
+++ Java/src/mideum/_46/Permutations.java
@@ -1,6 +1,6 @@
 package mideum._46;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -21,14 +21,53 @@ import java.util.List;
  *        ]
  */
 
-// TODO
 public class Permutations {
 
     public List<List<Integer>> permute(int[] nums) {
 
-        List<List<Integer>> lists = new ArrayList<>();
-       
+        List<List<Integer>> lists = new LinkedList<>();
 
-        return null;
+        if (nums == null) {
+            return lists;
+        }
+
+        List<Integer> list = new LinkedList<>();
+        list.add(nums[0]);
+        lists.add(list);
+
+        if (nums.length == 1) {
+            return lists;
+        }
+
+        for (int i = 1; i < nums.length; i ++) {
+            lists = addN(lists, nums[i]);
+        }
+
+        return lists;
+
     }
+
+
+    public List<List<Integer>> addN(List<List<Integer>> lists, int n) {
+
+        List<List<Integer>> listList = new LinkedList<>();
+
+        for (List<Integer> list : lists) {
+            int size = list.size();
+            for (int i = 0 ; i <= size; i ++ ) {
+                List<Integer> integerList = new LinkedList<>(list);
+                integerList.add(i, n);
+                listList.add(integerList);
+            }
+        }
+
+        return listList;
+    }
+
+    public static void main(String[] args) {
+
+        new Permutations().permute(new int[]{1, 2, 3});
+
+    }
+
 }
