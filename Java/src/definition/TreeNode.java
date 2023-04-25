5e678886ccda2984e51ccea4f18b86994193dd6c
LarryZeta <i@larryzeta.cc>
2023-04-25 17:58:18 +0800

    :building_construction: new TreeNode constructor

--- Java/src/definition/TreeNode.java
+++ Java/src/definition/TreeNode.java
@@ -1,5 +1,7 @@
 package definition;
 
+import java.util.LinkedList;
+import java.util.Queue;
 /**
  * Definition for a binary tree node.
  */
@@ -9,4 +11,36 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
     public TreeNode(int x) { val = x; }
+
+    public TreeNode(Integer... a) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        boolean left = true;
+        for (Integer i : a) {
+            if (i == null) {
+                left = !left;
+                if (left) {
+                    queue.remove();
+                }
+                continue;
+            }
+            TreeNode node = new TreeNode(i);
+            if (queue.isEmpty()) {
+                this.val = i;
+                queue.add(this);
+                continue;
+            }
+            TreeNode now = queue.peek();
+            if (left) {
+                now.left = node;
+                queue.add(node);
+                left = false;
+            } else {
+                now.right = node;
+                queue.add(node);
+                left = true;
+                queue.remove();
+            }
+        }
+    }
+
 }
